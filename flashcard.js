9609f8219e15fabef95d727d5baa649e6a41a61c
Neil van Beinum <neil.van.beinum@unboxedconsulting.com>
2020-04-11 11:36:34 +0100

    Add background and flip effect

--- flashcard.js
+++ flashcard.js
@@ -1,1 +1,25 @@
-console.log('it works');
+const flipButton = document.querySelector('.flip');
+const cardText = document.querySelector('.card-text');
+const card = document.querySelector('.inner');
+
+function randomBetween(lower, upper) {
+  return Math.random() * (upper - lower) + lower;
+}
+
+function flipCard(e) {
+  const { flipSideText } = card.dataset;
+
+  card.classList.toggle('flipped');
+
+  card.setAttribute('style', `--rotationDeg: ${randomBetween(-1, 1)}deg`);
+
+  card.removeChild(cardText);
+
+  card.dataset.flipSideText = cardText.value;
+
+  cardText.value = flipSideText;
+
+  card.appendChild(cardText);
+}
+
+flipButton.addEventListener('click', flipCard);
