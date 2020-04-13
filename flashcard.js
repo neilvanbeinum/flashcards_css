3f01989cecbbe8ee9c17f1c08fa210b544bd4c96
Neil van Beinum <neil.van.beinum@unboxedconsulting.com>
2020-04-13 18:01:29 +0100

    Improve tabbing behaviour
    Allow tabbing to toggle between front and back card inputs.

--- flashcard.js
+++ flashcard.js
@@ -1,6 +1,11 @@
 const flipButton = document.querySelector('.flip-button');
 const card = document.querySelector('.inner');
 
+const cardFrontText = document.querySelector('.front input[type="text"]');
+const cardBackText = document.querySelector('.back input[type="text"]');
+
+let lastElementWithFocus;
+
 function randomBetween(lower, upper) {
   return Math.random() * (upper - lower) + lower;
 }
@@ -11,3 +16,32 @@ function flipCard() {
 }
 
 flipButton.addEventListener('click', flipCard);
+
+function cardFocusing(e) {
+  if (!e.key.includes('Tab')) {
+    return;
+  }
+
+  lastElementWithFocus = document.querySelector(':focus');
+}
+
+document.addEventListener('keydown', cardFocusing);
+
+cardBackText.addEventListener('focus', () => {
+  if (
+    (lastElementWithFocus === flipButton ||
+      lastElementWithFocus === cardFrontText) &&
+    !card.classList.contains('flipped')
+  ) {
+    flipCard();
+  }
+});
+
+cardFrontText.addEventListener('focus', () => {
+  if (
+    lastElementWithFocus === cardBackText &&
+    card.classList.contains('flipped')
+  ) {
+    flipCard();
+  }
+});
